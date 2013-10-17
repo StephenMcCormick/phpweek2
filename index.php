8db383ec0fce0b74556be4f1a6eaab7eed11a275
001285489 <001285489@N206B1.labs.neit.edu>
2013-10-16 22:30:49 -0400

    week 2

--- index.php
+++ index.php
@@ -8,10 +8,19 @@
     <body>
         
         <?php
-
+            $usernameValue = "";
+            $passwordValue = "";
+            
+            $usernameErr = "";
+            $passwordErr = "";
         ?>
         
-        
+        <form name="mainform" method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">            
+            <label>UserName</label> <input type="text" name="username" value="<?php echo $usernameValue;?>" /> <?php echo $usernameErr ?> <br />
+            <label>Password</label> <input type="password" name="password" value="" /> <?php echo $passwordErr ?> <br />
+            
+            <input type="submit" value="Submit" />
+        </form>
         
     </body>
 </html>
